9832afb170730ad86edc40ebfff94794d5d8895f
Bruno Gonçalves de Alencar <brunoalencar2002@hotmail.com>
2022-08-06 12:29:09 -0300

    first commit

---
+++ spring-data-academia/src/main/java/com/dio/academia/springdataacademia/SpringDataAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.academia.springdataacademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAcademiaApplication.class, args);
+	}
+
+}
---
+++ spring-data-academia/src/test/java/com/dio/academia/springdataacademia/SpringDataAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.academia.springdataacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
